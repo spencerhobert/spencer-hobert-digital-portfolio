6dfc8c9b7cb555b65b0d018d72f69a277f1cc8c9
Spencer Hobert <spencerhobert@gmail.com>
2025-06-07 15:48:27 -0600

    Added Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,41 @@
-import Navbar from "./pages/Navbar/Navbar";
-import Header from "./pages/Header/Header";
+import Navbar from './pages/Navbar/Navbar'
+import Header from './pages/Header/Header'
 
 export default function App() {
-  return (
-    // <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-    //   <div className="text-center">
-    //     <h1 className="text-4xl font-bold text-gray-900 mb-8">
-    //       My React App
-    //     </h1>
-    //     <div className="bg-white p-8 rounded-lg shadow-md">
-    //       <button 
-    //         onClick={() => setCount((count) => count + 1)}
-    //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    //       >
-    //         Count is {count}
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
+    return (
+        // <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        //   <div className="text-center">
+        //     <h1 className="text-4xl font-bold text-gray-900 mb-8">
+        //       My React App
+        //     </h1>
+        //     <div className="bg-white p-8 rounded-lg shadow-md">
+        //       <button
+        //         onClick={() => setCount((count) => count + 1)}
+        //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        //       >
+        //         Count is {count}
+        //       </button>
+        //     </div>
+        //   </div>
+        // </div>
 
-    <div>
-      <Navbar />
-      <div className="grid grid-cols-10 min-h-screen mx-auto">
-        <div className="hidden md:block md:col-span-1 lg:col-span-2">
-          {/* Left sidebar */}
+        <div>
+            <Navbar />
+            <div className="mx-auto grid min-h-screen grid-cols-10">
+                <div className="hidden md:col-span-1 md:block lg:col-span-2">
+                    {/* Left sidebar */}
+                </div>
+                <div className="col-span-10 flex h-full flex-col gap-4 md:col-span-8 lg:col-span-6">
+                    <div className="h-96 min-h-32 bg-red-500">
+                        <Header />
+                    </div>
+                    <div className="flex-1 border bg-white">Main content</div>
+                    <div className="h-20 bg-blue-300">Footer</div>
+                </div>
+                <div className="hidden md:col-span-1 md:block lg:col-span-2">
+                    {/* Right sidebar */}
+                </div>
+            </div>
         </div>
-        <div className="lg:col-span-6 md:col-span-8 col-span-10 flex flex-col h-full gap-4">
-          <div className="h-96 bg-red-500 min-h-32">
-            <Header />
-          </div>
-          <div className="flex-1 bg-white border">
-            Main content
-          </div>
-          <div className="h-20 bg-blue-300">
-            Footer
-          </div>
-        </div>
-        <div className="hidden md:block md:col-span-1 lg:col-span-2">
-          {/* Right sidebar */}
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,38 +1,39 @@
-import { useState, useEffect } from "react";
-import forestImg from "./images/forest-trees.png";
+import { useState, useEffect } from 'react'
+import forestImg from './images/forest-trees.png'
 
 export default function Header() {
-
     const jobTitles: string[] = [
-        "Software Engineer",
-        "Computer Science Graduate",
-        "Full Stack Engineer",
-        "Web Developer",
-        "Backend Engineer",
-        "Software Developer",
-        "Frontend Engineer",
-        "Computer Science Graduate",
-        "Video Game Enthusiast",
-        "Husband",
-        "Dog Dad"
-    ];
+        'Software Engineer',
+        'Computer Science Graduate',
+        'Full Stack Engineer',
+        'Web Developer',
+        'Backend Engineer',
+        'Software Developer',
+        'Frontend Engineer',
+        'Computer Science Graduate',
+        'Video Game Enthusiast',
+        'Husband',
+        'Dog Dad',
+    ]
 
-    const [shownJobTitleIndex, setShownJobTitleIndex] = useState(0);
-    useEffect(() => { // Flip between job titles
+    const [shownJobTitleIndex, setShownJobTitleIndex] = useState(0)
+    useEffect(() => {
+        // Flip between job titles
         const interval = setInterval(() => {
-            setShownJobTitleIndex(idx => (idx + 1) % jobTitles.length);
-        }, 2500);
+            setShownJobTitleIndex((prev) => (prev + 1) % jobTitles.length)
+        }, 2500)
 
-        return () => clearInterval(interval);
-    }, [jobTitles.length]);
+        return () => clearInterval(interval)
+    }, [jobTitles.length])
 
     return (
-        <div className="h-full flex text-center items-center justify-center text-white text-shadow-black text-shadow-sm font-bold shadow-2xl object-cover"
-                style={{ backgroundImage: `url(${forestImg})`, objectFit: "cover" }}
+        <div
+            className="flex h-full items-center justify-center object-cover text-center font-bold text-white shadow-2xl text-shadow-black text-shadow-sm"
+            style={{ backgroundImage: `url(${forestImg})`, objectFit: 'cover' }}
         >
             Hello, I'm Spencer Hobert
             <br />
             {jobTitles[shownJobTitleIndex]}
         </div>
-    );
+    )
 }
